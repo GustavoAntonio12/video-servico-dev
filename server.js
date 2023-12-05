4d46c1a6b8ebdc132dec514ae687c2f880361b0a
Gustavo Antonio <99812422+GustavoAntonio12@users.noreply.github.com>
2023-12-04 21:37:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal);
+		io.to(data.to).emit("callAccepted", data.signal, data.horaInicio);
 	});
 });
 
